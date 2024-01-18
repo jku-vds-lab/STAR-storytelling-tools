0ea4715bdb7445a00c5ed6adb839bd00b2ad74ac
Shivam Agarwal <73848193+s-agarwl@users.noreply.github.com>
2024-01-18 16:32:38 +0100

    Updating repo with annotated bib entries

--- electron/index.js
+++ electron/index.js
@@ -13,9 +13,7 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
+            nodeIntegration: true
         },
         autoHideMenuBar: true
     })
